b09c92d05c62e88c083b0a938ae0b3313431f13a
Your Name <student06@dee2-dataprocess1.novalocal>
2020-04-16 10:54:37 +1000

    Created new R script to download and read in tabular data file in R. Update to the README.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # duki
 In class practice how to use Github
+
+## Contents
+* Readme file in markdown
+* Example R script
+
+## Motivation
+Help students learn about Github ad markdown format.
+
+## Installation
+R studio Version 1.2.1335 and R version 3.6.3
+
+## Credits
+Dulini Fernando
---
+++ example5 .R
@@ -0,0 +1,10 @@
+# Download the data
+download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part1_files/mydata.tsv", destfile = "mydata.tsv")
+
+# Read into R making sure the 1st columns is set as the row names and headers are recognized
+x <- read.table("mydata.tsv", header = TRUE , stringsAsFactors = FALSE, row.names = 1) 
+
+# Lets check if the data has been imported properly
+head(x)
+str(x)
+
